de5c5c2eee0a03e24c27b813b29994ba4b0b3828
vo-ke <godsimevoke1@gmail.com>
2023-03-03 20:21:28 +0100

    Navbar deploy

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+        padding: "2 rem"
+    },
+    Fontfamily: {
+      quintessential: ['Quintessential'],
+
+    }
+    
+  },
+
+  plugins: [],
+}
